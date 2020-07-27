acfe0f694414d8c3b7d04947efc504f26107671b
stephen66778899@gmail.com <stephenduan@Stephens-MacBook-Pro.local>
2020-07-27 11:16:17 -0700

    Header & Footer

--- React/confusion/src/components/DishdetailComponent.js
+++ React/confusion/src/components/DishdetailComponent.js
@@ -1,13 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody,
     CardTitle } from 'reactstrap';
 
-class DishDetail extends Component{
-    constructor(props){
-        super(props);
-    }
-
-    renderDish(dish){
+    function RenderDish({dish}){
         if(dish != null){
             return(
                 <Card>
@@ -26,7 +21,7 @@ class DishDetail extends Component{
         }
     }
 
-    renderComments(comments){
+    function RenderComments({comments}){
         const commentsList = comments.map((comment) => {
             return (
                 <li key={comment.id}>
@@ -48,15 +43,15 @@ class DishDetail extends Component{
         );
     }
 
-    render(){
-        if(this.props.dish != null){
+    const DishDetail = (props) => {
+        if(props.dish != null){
             return(
                 <div className="row">
                     <div className="col-12 col-md-5 m-1">
-                        {this.renderDish(this.props.dish)}
+                        <RenderDish dish={props.dish} />
                     </div>
                     <div className="col-12 col-md-5 m-1">
-                        {this.renderComments(this.props.dish.comments)}
+                        <RenderComments comments={props.dish.comments}/>
                     </div>
                 </div>            
             );
@@ -67,6 +62,5 @@ class DishDetail extends Component{
             );
         }
     }
-}
 
 export default DishDetail;
--- React/confusion/src/components/MainComponent.js
+++ React/confusion/src/components/MainComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
 import DishDetail from './DishdetailComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 
 class Main extends Component {
   constructor(props){
@@ -25,15 +26,12 @@ class Main extends Component {
     // App passes DISHES to Menu as an attribute, Menu may have access to it as a prop
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar> 
+        <Header />
         <div className="container">
             <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
             <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
         </div>
+        <Footer />
       </div>
     );
   }
