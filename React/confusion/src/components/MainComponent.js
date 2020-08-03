59cde7aebbab8cc57f96191ef8264f07b6e724fa
stephen66778899@gmail.com <stephenduan@Stephens-MacBook-Pro.local>
2020-08-02 19:09:15 -0700

    react redux form

--- React/confusion/src/components/MainComponent.js
+++ React/confusion/src/components/MainComponent.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-import { DISHES } from '../shared/dishes';
-import { COMMENTS } from '../shared/comments';
-import { LEADERS } from '../shared/leaders';
-import { PROMOTIONS } from '../shared/promotions';
-import {Switch, Route, Redirect} from 'react-router-dom';
+import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
+import { connect } from 'react-redux';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
@@ -12,27 +9,29 @@ import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import About from './AboutComponent';
 
+// neccessary for redux, state here, is the state from store, which is returned by Reducer in reducer.js
+// the returned object will be connected to Main component by the connect function, they will become props in Main
+const mapStateToProps = state => {
+  return {
+    dishes: state.dishes,
+    comments: state.comments,
+    promotions: state.promotions,
+    leaders: state.leaders
+  } 
+}
 
 class Main extends Component {
   constructor(props){
     super(props);
-
-    // DISHES here is lfted into the App.js, now menu can have access to it too
-    this.state = { 
-        dishes: DISHES,
-        comments: COMMENTS,
-        promotions: PROMOTIONS,
-        leaders: LEADERS
-    };
   }
 
   render(){
     // the dish, promotion and leader props get the dish with dish, promotion and leader that has the featured value to be true
     const HomePage = () => {
         return(
-            <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-                promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-                leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+            <Home dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+                promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+                leader={this.props.leaders.filter((leader) => leader.featured)[0]}
                 />
         );
     }
@@ -43,8 +42,8 @@ class Main extends Component {
     // therefore it is match.params.dishId
     const DishWithId = ({match}) => {
         return(
-            <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
-                comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}/>
+            <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}/>
         );
     }
 
@@ -62,10 +61,10 @@ class Main extends Component {
         <Header />
         <Switch>
             <Route path="/home" component={HomePage} />
-            <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+            <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes} />} />
             <Route path="/menu/:dishId" component={DishWithId} />
             <Route exact path="/contactus" component={Contact} />
-            <Route path="/aboutus" component={() => <About leaders={this.state.leaders} />}/>
+            <Route path="/aboutus" component={() => <About leaders={this.props.leaders} />}/>
             <Redirect to="/home" />
         </Switch>
         <Footer />
@@ -73,4 +72,6 @@ class Main extends Component {
     );
   }
 }
-export default Main;
+
+// connect the Main component with the store, withRouter is used because router is used in Main component
+export default withRouter(connect(mapStateToProps)(Main));
